550fcbe7d27ece19376f15d96d6cf301f228b4bd
Mohamed Salah <mohamed.abdelhalim@breadfast.com>
2025-06-15 10:50:25 +0300

    add packages

---
+++ Factor-Modularization-Issue/Factor-Modularization-Issue/Factor_Modularization_IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Factor_Modularization_IssueApp.swift
+//  Factor-Modularization-Issue
+//
+//  Created by Mohamed Salah on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Factor_Modularization_IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
