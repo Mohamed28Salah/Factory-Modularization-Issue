46231dc7855f2a55d2cccfb97b803ac871f99434
Mohamed Salah <mohamed.abdelhalim@breadfast.com>
2025-06-15 11:19:22 +0300

    import domain

--- Data/Package.swift
+++ Data/Package.swift
@@ -11,11 +11,15 @@ let package = Package(
             name: "Data",
             targets: ["Data"]),
     ],
+    dependencies: [
+        .package(path: "../Domain")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Data"),
+            name: "Data",
+            dependencies: ["Domain"]),
         .testTarget(
             name: "DataTests",
             dependencies: ["Data"]
--- Presentaion/Package.swift
+++ Presentaion/Package.swift
@@ -11,11 +11,15 @@ let package = Package(
             name: "Presentaion",
             targets: ["Presentaion"]),
     ],
+    dependencies: [
+        .package(path: "../Domain")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Presentaion"),
+            name: "Presentaion",
+            dependencies: ["Domain"]),
         .testTarget(
             name: "PresentaionTests",
             dependencies: ["Presentaion"]
